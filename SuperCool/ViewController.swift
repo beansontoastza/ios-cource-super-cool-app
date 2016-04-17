38ac34cd847356748997f98d79971c57bb116625
Renier Oosthuizen <oosthuizen.renier@gmail.com>
2016-04-17 08:12:22 +0200

    Initial Commit

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Renier Oosthuizen on 2016/04/17.
+//  Copyright © 2016 Beans On Toast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
